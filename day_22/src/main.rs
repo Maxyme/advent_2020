09d4bf9db0df8f87a754975dd2bfdcdaa907c355
Maxime Jacques <maxyme@outlook.com>
2021-02-25 09:36:19 -0500

    Day 22 - advent calendar - Part 2

--- day_22/src/main.rs
+++ day_22/src/main.rs
@@ -1,20 +1,81 @@
 // Day 22 - Calculate the winning player's score in a combat card game
 
 use regex::Regex;
-use std::collections::VecDeque;
+use std::collections::hash_map::DefaultHasher;
+use std::collections::{HashSet, VecDeque};
 use std::fs;
+use std::hash::{Hash, Hasher};
+use std::iter::FromIterator;
 
-//fn recursive_combat(deck_1: &mut VecDeque<usize>, deck_2: &mut VecDeque<usize>) -> (VecDeque<usize>, VecDeque<usize>) {
 fn recursive_combat<'a>(
     deck_1: &'a mut VecDeque<usize>,
     deck_2: &'a mut VecDeque<usize>,
 ) -> Vec<&'a usize> {
-    // Recursively peform a game of combat with added rules
+    // Recursively perform a game of combat with added rules
+    fn recursive_combat<'a>(
+        deck_1: &'a mut VecDeque<usize>,
+        deck_2: &'a mut VecDeque<usize>,
+        seen_decks: &mut HashSet<u64>,
+    ) -> (&'a mut VecDeque<usize>, &'a mut VecDeque<usize>) {
+        loop {
+            if &mut deck_1.len() == &0 || &mut deck_2.len() == &0 {
+                break;
+            }
 
-    // fn recursive_combat<'a>(deck_1: &'a mut VecDeque<usize>, deck_2: &'a mut VecDeque<usize>) -> Vec<&'a usize> {
-    //
-    // }
-    //
+            // check if there was a previous round with the same cards, then deck 1 wins
+            let mut hash = DefaultHasher::new();
+            (&deck_1, &deck_2).hash(&mut hash);
+            let desk_hashes = hash.finish();
+
+            if seen_decks.contains(&desk_hashes) {
+                // only the length matters, so return the longest deck first
+                return if deck_1.len() > deck_2.len() {
+                    (deck_1, deck_2)
+                } else {
+                    (deck_2, deck_1)
+                };
+            } else {
+                seen_decks.insert(desk_hashes);
+            }
+
+            // pick the first one one top and compare
+            let card_1 = deck_1.pop_front().expect("Value Error");
+            let card_2 = deck_2.pop_front().expect("Value Error");
+
+            if deck_1.len() >= card_1 && deck_2.len() >= card_2 {
+                // Make new vecdeques from the previous ones: todo: probably a more efficient way of doing this
+                let deck_1_iter = deck_1.clone().into_iter().take(card_1);
+                let mut new_deck_1: VecDeque<usize> = VecDeque::from_iter(deck_1_iter);
+                let deck_2_iter = deck_2.clone().into_iter().take(card_2);
+                let mut new_deck_2: VecDeque<usize> = VecDeque::from_iter(deck_2_iter);
+
+                // Send a new hashset to the recursive function
+                let mut new_seen_decks: HashSet<u64> = HashSet::new();
+                let (new_deck_1, new_deck_2) =
+                    recursive_combat(&mut new_deck_1, &mut new_deck_2, &mut new_seen_decks);
+                if new_deck_1.len() > new_deck_2.len() {
+                    // deck_1 won with it's card
+                    deck_1.push_back(card_1);
+                    deck_1.push_back(card_2);
+                } else {
+                    deck_2.push_back(card_2);
+                    deck_2.push_back(card_1);
+                }
+            } else {
+                // normal combat
+                if card_1 > card_2 {
+                    deck_1.push_back(card_1);
+                    deck_1.push_back(card_2);
+                } else {
+                    deck_2.push_back(card_2);
+                    deck_2.push_back(card_1);
+                }
+            }
+        }
+        return (deck_1, deck_2);
+    }
+    let mut seen_decks: HashSet<u64> = HashSet::new();
+    let (deck_1, deck_2) = recursive_combat(deck_1, deck_2, &mut seen_decks);
     if deck_1.len() > deck_2.len() {
         deck_1.iter().collect()
     } else {
@@ -77,7 +138,7 @@ fn main() {
     let mut player_1_deck = parse_values(input_entries[0]);
     let mut player_2_deck = parse_values(input_entries[1]);
 
-    // Part 2 - normal combat
+    // Part 1 - normal combat
     let best_deck = combat(&mut player_1_deck, &mut player_2_deck);
 
     // Calculate the winning player's score
